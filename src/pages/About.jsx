001c75f92e741ea243219f14836b2cf4ea0ef085
Koh_Hui_Lyn <2b01.2021672.kohhuilyn@gmail.com>
2023-01-20 11:46:03 +0800

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>DA About</div>
+  )
+}
+
+export default About
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>DA Contact</div>
+  )
+}
+
+export default Contact
